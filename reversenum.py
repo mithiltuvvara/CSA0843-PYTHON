39437a332a6772119ca444b7e7f48713c8af732a
MITHIL <112242491+mithiltuvvara@users.noreply.github.com>
2022-09-26 10:54:22 +0530

    Create reversenum.py

---
+++ reversenum.py
@@ -0,0 +1,10 @@
+um  = int(input("enter integer to display it in  reverse="))
+reverse = 0
+remainder = 0
+
+while num != 0:
+    remainder = num % 10
+    reverse = reverse * 10 + remainder
+    num //= 10
+
+print("Reversed Number: " +str(reverse))r
